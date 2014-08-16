90f0bc3edbad3070a034a2c4e59ff872cd9fe54e
Chris Ward <chriswhward@gmail.com>
2014-08-15 20:50:37 -0700

    Removed title from template

--- wp-content/themes/webbie-package/webbie/header.php
+++ wp-content/themes/webbie-package/webbie/header.php
@@ -88,8 +88,8 @@ get_template_part( 'template-parts/cake_contact-setup');
 
 	<hgroup class="site_titles">
 		<h1 <?php if ( $NHP_Options->get('logo_image') ) { ?>class="has_logo_icon" <?php } ?>>
-<!-- 			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> " rel="home">
- -->
+
+
 				<?php // logo image icon
 				if ( $NHP_Options->get('logo_image') ) { ?>
 					<img src="<?php $NHP_Options->show('logo_image'); ?>" alt="<?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?>" class="logo_icon" />
